18b8c4d3938007c05ed5612fd77b91fb4e2a47eb
Alexandra Østermark <alex.cramt@gmail.com>
2020-10-01 09:28:01 +0200

    initial

---
+++ scripts/get_char_count.rb
@@ -0,0 +1,25 @@
+require "json"
+require "pdf-reader"
+
+if ARGV.include?("--fast")
+    Process.detach(Process.spawn("ruby " + __FILE__))
+else
+    latex_file = "small_master"
+
+    command = JSON.parse(File.read(".vscode/settings.json"))["latex-workshop.latex.tools"].find {|x| x["name"] == "pdflatex"}
+    args = command["args"]
+    command = command["command"]
+    args = args.map {|x| x == "%DOC%" ? latex_file + ".tex" : x}
+    `#{command} #{args.join(" ")}`
+
+    if(File.exist?(latex_file + ".pdf"))
+        pdf = PDF::Reader.new(latex_file + ".pdf")
+        char_count = pdf.pages.map {|x| x.text}.join(" ").gsub(/\s+/, " ").length
+        File.write("current_char_count.tmp", char_count)
+        File.write("current_page_count.tmp", (char_count / 2400).round)
+    else
+        File.write("current_char_count.tmp", 0)
+        File.write("current_page_count.tmp", 0)
+    end
+end
+exit
